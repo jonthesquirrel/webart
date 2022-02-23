9c0c3f145c2e2f9f62ee2d45eae091f62073e6b8
Jonathan Herman <jdh11235@gmail.com>
2022-02-23 07:55:25 -0600

    Remove DOM-based rain text, close #13

--- raining/components/Rain.vue
+++ raining/components/Rain.vue
@@ -1,65 +1,13 @@
 <script setup>
 import { onMounted } from 'vue'
-import Logger from './Logger.vue'
-import { sample } from 'lodash-es'
 
 onMounted(() => {
     const characterPossibilities = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.split('')
-
-    const getViewportWidthInCharacters = () => Math.ceil(window.innerWidth / parseFloat(window.getComputedStyle(document.body).fontSize) * 2.05)
-    const getViewportHeightInCharacters = () => Math.ceil(window.innerHeight / parseFloat(window.getComputedStyle(document.body).fontSize) * 1.1)
-    // not completely accurate but close enough
-
-    let trails = []
-
-    const debugText = (text) => {
-        document.querySelector('#logger').innerText = text
-    }
-
-    const addTrail = () => {
-        let trail = {
-            element: document.createElement('div')
-        }
-        trail.element.classList.add('trail')
-        document.querySelector('.rain').appendChild(trail.element)
-        for (let i = 0; i < getViewportHeightInCharacters(); i++) {
-            let character = document.createElement('span')
-            character.classList.add('character')
-            character.innerText = sample(characterPossibilities)
-            trail.element.appendChild(character)
-        }
-        trails.push(trail)
-    }
-
-    const removeTrail = () => {
-        let trail = trails.pop()
-        trail.element.remove()
-    }
-
-    // will this have race conditions?
-    const updateTrails = () => {
-        const currentWidth = trails.length
-        const viewportWidth = getViewportWidthInCharacters()
-        debugText(`currentWidth: ${currentWidth} / viewportWidth: ${viewportWidth}`)
-        if (currentWidth < viewportWidth) {
-            for (let i = currentWidth; i < viewportWidth; i++) {
-                addTrail()
-            }
-        } else if (currentWidth > viewportWidth) {
-            for (let i = currentWidth; i > viewportWidth; i--) {
-                removeTrail()
-            }
-        }
-    }
-
-    updateTrails()
-    setInterval(updateTrails, 1000)
 })
 </script>
 
 <template>
     <div class="rain">
-        <Logger id="logger"></Logger>
     </div>
 </template>
 
