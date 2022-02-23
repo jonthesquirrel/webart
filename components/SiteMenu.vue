88f578c41683c81496cb0f4045af375d70776516
Jonathan Herman <jdh11235@gmail.com>
2022-02-22 23:50:44 -0600

    Turn site menu into a vue component

---
+++ components/SiteMenu.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="menu">
+        <a href="/">&lt; Squirrel's Web Art</a>
+    </nav>
+</template>
+
+<style scoped>
+.menu {
+    color: white;
+    font-family: sans-serif;
+    left: 0vw;
+    position: fixed;
+    top: 2rem;
+    z-index: 1;
+}
+
+.menu a {
+    background-color: black;
+    color: white;
+    padding: 0.8rem;
+    text-decoration: none;
+    vertical-align: middle;
+}
+
+.menu a:hover {
+    text-decoration: underline;
+}
+</style>
--- raining/App.vue
+++ raining/App.vue
@@ -1,3 +1,8 @@
+<script setup>
+import SiteMenu from '../components/SiteMenu.vue'
+</script>
+
 <template>
+    <SiteMenu></SiteMenu>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci laborum aliquam eveniet distinctio a veniam quibusdam eligendi ipsum saepe nemo. Architecto deleniti culpa ut incidunt voluptatibus quisquam quae fuga tenetur?</p>
 </template>
