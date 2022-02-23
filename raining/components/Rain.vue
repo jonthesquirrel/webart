2a0f1c607f7e7e9666c84f26af37e7875a7aed3c
Jonathan Herman <jdh11235@gmail.com>
2022-02-23 01:22:08 -0600

    Rewrite rain trails without components for better performance, but looks messy

--- raining/components/Rain.vue
+++ raining/components/Rain.vue
@@ -1,16 +1,29 @@
 <script setup>
-import Trail from './Trail.vue'
+import { onMounted } from 'vue';
+onMounted(() => {
+    for (let i = 0; i < 200; i++) {
+        let trail = document.createElement('div')
+        trail.classList.add('trail')
+        document.querySelector('.rain').appendChild(trail)
+        for (let i = 0; i < 100; i++) {
+            let letter = document.createElement('span')
+            letter.classList.add('letter')
+            letter.innerText = 'L'
+            trail.appendChild(letter)
+        }
+    }
+})
 </script>
 
 <template>
     <div class="rain">
-        <Trail v-for="n in 200"></Trail>
+
     </div>
 </template>
 
 <style scoped>
 .rain {
-    overflow: hidden;
-    width: auto;
+    color: limegreen;
+    font-family: monospace;
 }
 </style>
